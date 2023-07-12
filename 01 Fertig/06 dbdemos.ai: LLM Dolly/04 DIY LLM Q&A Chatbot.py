b998926112adf3bd3d49bdaeb62a7e1eefda26bb
arausch2023 <arausch2023>
2023-07-12 06:57:31 +0000

    DIY QA Chatbot ergänzt
    DIY QA Chatbot ergänzt

---
+++ 01 Fertig/06 dbdemos.ai: LLM Dolly/04 DIY LLM Q&A Chatbot.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Externe Daten an Chat GPT 3.5 Turbo anbinden um einen domänenspezifischen Chatbot mit REST API und Serving Endpoint zu erstellen 
